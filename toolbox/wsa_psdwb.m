d19f5a0393dae763db5250ef86d177caa5ac4218
DG28828 <dannygarro28801@gmail.com>
2026-01-28 17:47:42 -0600

    Agregado manejo de entradas para entradas requeridas y entradas opcionales de la función, así como los valores por defecto. Además se agregó el cálculo del psd normal y el cspd en la misma función.

--- toolbox/wsa_psdwb.m
+++ toolbox/wsa_psdwb.m
@@ -1,26 +1,80 @@
-function [Ixy, W] = wsa_psdwb(X, Y, M, N0, K, N, ventana, Nfft)
-%wbpcsd - densidad espectral de potencia cruzada mediante método de Welch-Barlett.
+function [I, W] = wsa_psdwb(X, N, N0, ventana, varargin)
+%wsa_psdwb - densidad espectral de potencia mediante el método de Welch-Barlett.
 %
-% Esta función realiza la estimación espectral cruzada de X e Y mediante la técnica de
-% Welch-Barlett.
+%   Esta función realiza la estimación de la densidad espectral de potencia 
+%   de X mediante el método de Welch-Barlett.
+%   Si se especifica Y, se calcula la densidad espectral de potencia cruzada 
+%   de X e Y.
 %
-% Sintaxis:
-% [PSD, W] = wbpsd(X, Y, M, N0, K, N, ventana, Nfft)
+%   Sintaxis:
+%       I = wsa_wbpsd(X, N, N0, ventana)
+%       I = wsa_wbpsd(X, N, N0, ventana, 'M', M, 'K', K, 'Nfft', Nfft, 'Y', Y)
 %
-% Argumentos de entrada:
-%   X - Arreglo de entrada X (vector)
-%   Y - Arreglo de entrada Y (vector)
-%   M - Longitud de la secuencia de entrada (entero)
-%   N0 - Longitud del solapamiento entre los segmentos
-%   K - Número de segmentos
-%   N - Longitud del segmento (entero potencia de 2)
-%   ventana - ventana a emplear (rectangular, von Hann, Hanning)
-%   Nfft - Longitud del periodograma del segmento (por defecto Nfft = 512)
+%       [I, W] = wsa_wbpsd(X, N, N0, ventana)
+%       [I, W] = wsa_wbpsd(X, N, N0, ventana, 'M', M, 'K', K, 'Nfft', Nfft, 'Y', Y)
 %
-% Argumentos de salida
-% I - Estimador del espectro de potencia (vector)
-% W - Frecuencias angulares (vector)
-% 
+%   Argumentos de entrada:
+%       X - Arreglo de entrada X 
+%           vector
+%       N - Longitud del segmento
+%           entero (potencia de 2)
+%       N0 - Longitud del solapamiento entre los segmentos
+%           entero
+%       ventana - ventana a emplear 
+%           string ("rectangular", "hann", "hamming")
+%       M - Longitud de la secuencia de entrada 
+%           entero | (opcional) Por defecto: M = longitud de X
+%       K - Número de segmentos
+%           entero | (opcional) Por defecto K = (M-N0)/(N-N0)
+%       Nfft - Longitud del periodograma del segmento
+%           entero (potencia de 2) | (opcional) Por defecto: máximo entre
+%           la potencia de 2 mayor mas cercana a N y 512.
+%       Y - Arreglo de entrada Y 
+%           vector | (opcional) Por defecto: []
+%
+%   Argumentos de salida:
+%       I - Estimador del espectro de potencia 
+%           vector
+%       W - Frecuencias angulares 
+%           vector
+%
+% -------------------------------------------------------------------------
+% Universidad de Costa Rica
+% Escuela de Ingeniería Civil
+% Autor: Danny Garro Arias
+% Fecha de creación: 28/01/2026
+% Fecha de modificación: 28/01/2026
+% -------------------------------------------------------------------------
+
+%% Manejo de entradas
+
+%Valores por defecto
+M_default = length(X);
+K_default = floor((M_default - N0)/(N - N0));
+Nfft_default = max(512, 2^nextpow2(N));
+
+%Input parser
+p = inputParser;
+
+addRequired(p, 'X');
+addRequired(p, 'N');
+addRequired(p, 'N0');
+addRequired(p, 'ventana');
+
+addParameter(p, 'M',    M_default);
+addParameter(p, 'K',    K_default);
+addParameter(p, 'Nfft', Nfft_default);
+addParameter(p, 'Y',    []);
+
+parse(p, X, N, N0, ventana, varargin{:});
+
+%Resultados
+M    = p.Results.M;
+K    = p.Results.K;
+Nfft = p.Results.Nfft;
+Y    = p.Results.Y;
+
+espectro_cruzado = ~isempty(Y);
 
 %% Verificaciones iniciales
 
@@ -95,22 +149,36 @@ U = mean(w.^2);
 % El codigo mostrado a continuación emplea esto ajustado a los índices de
 % MATLAB (comenzando en 1).
 
-I_xy_acum = zeros(Nfft, 1);
-
+I_acum = zeros(Nfft, 1);
 R = N-N0;
-for r = 1:K
 
-    % Señales enventanadas
-    x_r = X((r-1)*R+1:(r-1)*R+N).*w;
-    y_r = Y((r-1)*R+1:(r-1)*R+N).*w;
-
-    % Periodogramas
-    [H_x_r, W] = dtft(x_r, Nfft);
-    [H_y_r, ~] = dtft(y_r, Nfft);
-
-    I_xy_n = (1/(N*U))*H_x_r.*conj(H_y_r);
-   
-    I_xy_acum = I_xy_acum + I_xy_n;
+if ~espectro_cruzado
+    for r = 1:K
+    
+        % Señal enventanada
+        x_r = X((r-1)*R+1:(r-1)*R+N).*w;
+    
+        % Periodograma
+        [H_r, W] = dtft(x_r, Nfft);
+        I_n = (1/(N*U))*abs(H_r).^2;
+       
+        I_acum = I_acum + I_n;
+    end
+else
+    for r = 1:K
+    
+        % Señales enventanadas
+        x_r = X((r-1)*R+1:(r-1)*R+N).*w;
+        y_r = Y((r-1)*R+1:(r-1)*R+N).*w;
+    
+        % Periodogramas
+        [H_x_r, W] = dtft(x_r, Nfft);
+        [H_y_r, ~] = dtft(y_r, Nfft);
+    
+        I_xy_n = (1/(N*U))*H_x_r.*conj(H_y_r);
+       
+        I_acum = I_acum + I_xy_n;
+    end
 end
 
-Ixy = I_xy_acum./K;   % Periodograma promedio
+I = I_acum./K;   % Periodograma promedio
