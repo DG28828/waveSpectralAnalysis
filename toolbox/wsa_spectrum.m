53414a0b995ef6378f524c53183aa5f4523478a0
DG28828 <dannygarro28801@gmail.com>
2026-02-07 18:44:23 -0600

    Cambiado el nombre de la variable de entrada de eta a X, para que sea genérico.

--- toolbox/wsa_spectrum.m
+++ toolbox/wsa_spectrum.m
@@ -1,17 +1,17 @@
-function [out, info] = wsa_spectrum(eta, fs, varargin)
+function [out, info] = wsa_spectrum(X, fs, varargin)
 %wsa_spectrum - espectro de energía.
 %
-%   Esta función estima el espectro de energía de un registro de mediciones 
-%   de superficie libre (eta). El espectro calculado corresponde a la
-%   densidad espectral de potencia unilateral (frecuencias positiavs) del 
-%   registro medido. El espectro se calcula mediante el método de
-%   preiodogramas medio, siguiendo la metodología de Welch-Barlett
+%   Esta función estima el espectro de energía de una secuencia de 
+%   datos (X). El espectro calculado corresponde a la densidad espectral 
+%   de potencia unilateral (frecuencias positivas). El espectro se calcula 
+%   mediante el método de periodogramas medio, siguiendo la metodología de 
+%   Welch-Barlett
 %
 %   Sintaxis:
 %
 %
 %   Argumentos de entrada:
-%       eta - secuencia de superficie libre 
+%       x - secuencia de datos 
 %           vector
 %       fs - frecuencia de muestreo (Hz)
 %           entero
@@ -24,7 +24,7 @@ function [out, info] = wsa_spectrum(eta, fs, varargin)
 %
 %   Argumentos de salida:
 %       out - Salidas numéricas | struct
-%           S - estimador del espectro de energía unilateral [unidad de eta]^2/Hz
+%           S - estimador del espectro de energía unilateral [unidad de X]^2/Hz
 %               vector
 %           f - frecuencias físicas Hz
 %               vector
@@ -35,7 +35,7 @@ function [out, info] = wsa_spectrum(eta, fs, varargin)
 % Escuela de Ingeniería Civil
 % Autor: Danny Garro Arias
 % Fecha de creación: 30/01/2026
-% Fecha de modificación: 03/02/2026
+% Fecha de modificación: 07/02/2026
 % -------------------------------------------------------------------------
 
 %% Manejo de entradas
@@ -47,13 +47,13 @@ pc_default = 0;
 %Input parser
 p = inputParser;
 
-addRequired(p, 'eta');
+addRequired(p, 'X');
 addRequired(p, 'fs');
 
 addParameter(p, 'DoF', DoF_default);
 addParameter(p, 'pc',    pc_default);
 
-parse(p, eta, fs, varargin{:});
+parse(p, X, fs, varargin{:});
 
 %Resultados
 DoF    = p.Results.DoF;
@@ -62,8 +62,8 @@ pc     = p.Results.pc;
 %% Verificaciones iniciales
 
 %Verificar eta es vector columna
-if size(eta, 1) ~= length(eta)
-    eta = eta';
+if size(X, 1) ~= length(X)
+    X = X';
 end
 
 if DoF ~= DoF_default
@@ -85,14 +85,14 @@ end
 
 ventana = "hann";    
 K = DoF/2;
-Nfft = 2^nextpow2(5*(2*length(eta)/(K+1)));
-[out_pswb, info] = wsa_psdwb(eta, ventana, 'K', K, 'Nfft', Nfft, 'pc', pc);
+Nfft = 2^nextpow2(5*(2*length(X)/(K+1)));
+[out_pswb, info] = wsa_psdwb(X, ventana, 'K', K, 'Nfft', Nfft, 'pc', pc);
 I = out_pswb.I;
 W = out_pswb.W;
 %Convertir psd bilateral a espectro unilateral y convertir 
 % Conversión:
-%   PSD bilateral: [eta^2 / rad/muestra]
-%   PSD unilateral: [eta^2 / rad/s] = [eta^2 / Hz]
+%   PSD bilateral: [X^2 / rad/muestra]
+%   PSD unilateral: [X^2 / rad/s] = [eta^2 / Hz]
 S = I(W>=0)/fs;   
 S(2:end) = 2*S(2:end); %La componente DC (W=0) no se duplica
 f = fs*W(W>=0)/(2*pi);
@@ -105,10 +105,10 @@ f = fs*W(W>=0)/(2*pi);
 %Validación energética:
 %   Se verifica el cumplimiento de integral_0_inf(S(f)df) = varianza
 m0 = trapz(f, S);       %El momento de primer orden es el área bajo la curva
-var_eta = var(eta);     %Varianza de la señal de entrada
-error_relativo = 100*abs(m0-var_eta)/var_eta;
+varianza = var(X);     %Varianza de la señal de entrada
+error_relativo = 100*abs(m0-varianza)/varianza;
 if pc
-    fprintf('Validación energética:\n\t m0 = %.4f (área bajo la curva) \n\t varianza señal = %.4f \n\t error relativo = %.2f %%\n', m0, var_eta, error_relativo)
+    fprintf('Validación energética:\n\t m0 = %.4f (área bajo la curva) \n\t varianza señal = %.4f \n\t error relativo = %.2f %%\n', m0, varianza, error_relativo)
 end
 
 %Struct para resultados
@@ -118,7 +118,7 @@ out.f = f;
 
 %Agregar información adicional al struct de info
 info.fs = fs;
-info.var_eta = var_eta;  
+info.varianza = varianza;  
 info.m0 = m0;
 info.error_relativo = error_relativo;
 
