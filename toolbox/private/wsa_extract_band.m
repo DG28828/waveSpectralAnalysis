c5c7b356a4872f85d87d9ef61bd74f25fac7f838
DG28828 <dannygarro28801@gmail.com>
2026-03-21 16:21:47 -0600

    Se generalizó mas la función para ser utilizada por ambas funciones de parámetros espectrales y direccionales.

--- toolbox/private/wsa_extract_band.m
+++ toolbox/private/wsa_extract_band.m
@@ -1,5 +1,5 @@
-function [f_band, S_band] = wsa_extract_band(f, S, limits)
-%Extraer los valores de f y S correspondientes a los límites fmin y fmax de
+function [f_band, X_band] = wsa_extract_band(f, X, limits)
+%Extraer los valores de f y X correspondientes a los límites fmin y fmax de
 %la banda indicada
 
     fmin = limits(1);
@@ -8,5 +8,10 @@ function [f_band, S_band] = wsa_extract_band(f, S, limits)
     mask = (f > fmin) & (f <= fmax);
 
     f_band = f(mask);
-    S_band = S(mask);
+
+    if ~isempty(X)
+        X_band = X(mask);
+    else 
+        warning('El vector de entrada no tiene valores, estableciendo banda vacía')
+        X_band = [];
 end
